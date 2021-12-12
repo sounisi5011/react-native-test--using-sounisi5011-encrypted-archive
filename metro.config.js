2c81e78a0915c8c55ca83f99f96ee8406740a8ff
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2021-12-12 16:18:31 +0900

    fix build errors when using `.cjs` extension
    @sounisi5011/ts-utils-is-property-accessible uses the `.cjs` extension.
    It may not work because of this.
    see https://github.com/thysultan/stylis.js/issues/233#issuecomment-663635896

--- metro.config.js
+++ metro.config.js
@@ -3,7 +3,12 @@ module.exports = {
     extraNodeModules: {
       crypto: require.resolve('crypto-browserify'),
       stream: require.resolve('readable-stream'),
-    }
+    },
+    // @sounisi5011/ts-utils-is-property-accessible uses the `.cjs` extension.
+    // It may not work because of this.
+    // If the build fails, please add 'cjs' in resolver.sourceExts config.
+    // see https://github.com/thysultan/stylis.js/issues/233#issuecomment-663635896
+    sourceExts: [ 'ts', 'tsx', 'js', 'jsx', 'json', 'cjs'],
   },
 };
 
@@ -15,5 +20,6 @@ module.exports = {
 const { getDefaultConfig } = require('@expo/metro-config');
 const defaultConfig = getDefaultConfig(__dirname);
 const newConfig = module.exports;
+defaultConfig.resolver.sourceExts = [...new Set([...defaultConfig.resolver.sourceExts, ...newConfig.resolver.sourceExts])];
 Object.assign(defaultConfig.resolver.extraNodeModules, newConfig.resolver.extraNodeModules);
 module.exports = defaultConfig;
