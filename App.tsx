47c17f82c2c450f510ff78be73234a3a888c3257
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2021-12-12 15:17:26 +0900

    try to import `@sounisi5011/encrypted-archive`
    This will fail, because the [Node.js built-in `stream` module] does not exist.
    
    [Node.js built-in `stream` module]: https://nodejs.org/api/stream.html

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
+import { inspect } from 'util';
+
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import * as mod from '@sounisi5011/encrypted-archive';
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
+        <Text>mod: {inspect(mod)}</Text>
         <StatusBar style="auto" />
       </View>
     </SafeAreaProvider>
